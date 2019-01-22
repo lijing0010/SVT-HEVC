a5616153c90d2005d14062813f26b32a9edda42c
Jing Li <jing.b.li@intel.com>
2019-01-22 23:20:25 +0800

    Fix the dump recon issue for non-dlf situation
    Signed-off-by: Jing Li <jing.b.li@intel.com>

--- Source/Lib/Codec/EbCodingLoop.c
+++ Source/Lib/Codec/EbCodingLoop.c
@@ -3176,11 +3176,9 @@ EB_EXTERN void EncodePass(
     EB_BOOL enableStrongIntraSmoothing = sequenceControlSetPtr->enableStrongIntraSmoothing;
     CodingUnit_t **codedLeafArrayPtr = lcuPtr->codedLeafArrayPtr;
 
-    // This flag needs to be set true when SAO is enabled for Non reference pictures so that SAO uses filtered samples
-    EB_BOOL dlfEnableFlag = (EB_BOOL)(!sequenceControlSetPtr->staticConfig.disableDlfFlag && 
-        ///*(pictureControlSetPtr->ParentPcsPtr->isUsedAsReferenceFlag) kelvin*/1) ||
-        (pictureControlSetPtr->ParentPcsPtr->isUsedAsReferenceFlag)) ||
-        sequenceControlSetPtr->staticConfig.reconEnabled;
+    EB_BOOL dlfEnableFlag = (EB_BOOL)(!sequenceControlSetPtr->staticConfig.disableDlfFlag) &&
+        (pictureControlSetPtr->ParentPcsPtr->isUsedAsReferenceFlag ||
+        sequenceControlSetPtr->staticConfig.reconEnabled);
 
     dlfEnableFlag = contextPtr->allowEncDecMismatch ? EB_FALSE : dlfEnableFlag;
 
